3c1cad55efa00ba8f1fb1e8f9b3669853f1cbf3c
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-07 16:04:39 +0200

    add constant and event click

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const userName = document.getElementById ('name')
+const userKm = document.getElementById ('Km')
+const userAge = document.getElementById ('age')
+
+const basicPrice = 0.21
+const ticketPrice = userKm * basicPrice
+const euro = €
+
+const btnGenerator = document.querySelector('.btn_generator');
+
+btnGenerator.addEventListener('click', function () {
+      console.log(userName.value);
+      console.log(userKm.value);
+      console.log(age.value);
+});
