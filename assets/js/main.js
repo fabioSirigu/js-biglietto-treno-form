6481c7a0b8d1530f3a960dbab98ca48d93fc1887
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-07 17:20:45 +0200

    add final structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,44 @@
-const userName = document.getElementById ('name')
-const userKm = document.getElementById ('Km')
-const userAge = document.getElementById ('age')
+/* GENERAL CONSTANT */
+const userName = document.getElementById ('name').value;
+console.log(userName);
+const userKm = document.getElementById ('km').value;
+console.log(userKm);
+const userAge = document.getElementById ('age').value;
+console.log(userAge);
 
-const basicPrice = 0.21
-const ticketPrice = userKm * basicPrice
-const euro = '€'
+
+/* const userAgeSelected = userAge.options[userAge.selectedIndex].text;
+console.log(userAgeSelected);
+const minors = document.getElementById ('minors');
+const adult = document.getElementById ('adult');
+const over = document.getElementById ('over'); */
+
+
+const basicPrice = 0.21;
+let ticketPrice = userKm * basicPrice;
+const euro = '€';
+
+/* DISCOUNT CONSTANT */
+const salesMinors = (ticketPrice / 100) * 20;
+const salesOver = (ticketPrice / 100) * 40;
 
 const btnGenerator = document.querySelector('.btn_generator');
 
 btnGenerator.addEventListener('click', function () {
-      console.log(userName.value);
-      console.log(userKm.value);
-      console.log(age.value);
+
+      if (userAge < 18) {
+            ticketPrice = ticketPrice - salesMinors;
+            document.getElementById("discount").innerHTML = "-20%";       
+      } else if (userAge > 64) {
+            ticketPrice = ticketPrice - salesOver;
+            document.getElementById("discount").innerHTML = "-40%";
+      }
+
+      document.getElementById("final_price").innerHTML = `${ticketPrice.toFixed( 2 )} €`;
+      document.getElementById("ticket_name").innerHTML = userName;
+      document.getElementById("ticket_distance").innerHTML = userKm;
+      document.getElementById("discount").innerHTML = "/";
+      document.getElementById("current_age").innerHTML = userAge;
+
+
 });
